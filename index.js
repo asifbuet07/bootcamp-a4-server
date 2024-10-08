bcf996c4123417355b295a4ff29bf112bfb1e623
DESKTOP-54ATNKA\ASIF <asifbuet07@gmail.com>
2024-10-08 17:22:59 +0600

    Books API

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ const books = require("./data/books-data.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
+  res.send("Welcome to the server of Book Heaven!");
+});
+
+app.get("/api/books", (req, res) => {
   res.send(books);
+});
+
+app.get("/api/books/:id", (req, res) => {
+  const bookId = parseInt(req.params.id, 10);
+  const book = books.find((b) => b.bookId === bookId);
+
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).send({ message: "Book not found" });
+  }
 });
 
 app.listen(port, () => {
